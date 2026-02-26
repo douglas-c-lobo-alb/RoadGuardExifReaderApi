920a8c63ea122660b0f6edc2f3121d9fe72b2c7a
Douglas Lobo <douglas-c-lobo@alticelabs.com>
2026-02-26 15:21:17 +0000

    add Dtos/ directory and ImageInfoDto

---
+++ ExifApi/Dtos/ImageInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using MetadataExtractor;
+
+namespace ExifApi.Dtos;
+
+public class ImageInfoDto
+{
+    public string? FileName { get; set; }
+    public string? CameraMake { get; set; }
+    public string? CameraModel { get; set; }
+    public string? DateTaken { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+    public double? Altitude { get; set; }
+}
