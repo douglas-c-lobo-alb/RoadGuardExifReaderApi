683ba93073e459e9fe896f7296f9c66876351d12
Douglas Lobo <douglas-c-lobo@alticelabs.com>
2026-03-05 08:57:37 +0000

    refactor ExifService: extract single-item method, fix Latitude and Longitude to decimal

--- ExifApi/Services/ExifService.cs
+++ ExifApi/Services/ExifService.cs
@@ -36,45 +36,61 @@ public class ExifService
 
         foreach (var file in imagesFiles)
         {
-            try
-            {
-                var directories = ImageMetadataReader.ReadMetadata(file);
-                var fileName = Path.GetFileName(file);
-                var ifd0 = directories.OfType<ExifIfd0Directory>().FirstOrDefault();
-                var subIfd = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
-                var cameraMake = ifd0?.GetDescription(ExifDirectoryBase.TagMake);
-                var cameraModel = ifd0?.GetDescription(ExifDirectoryBase.TagModel);
-                var gps = directories.OfType<GpsDirectory>().FirstOrDefault();
-                var geo = gps is not null && gps.TryGetGeoLocation(out var loc) ? loc : (GeoLocation?)null;
-                var altitude = gps?.TryGetRational(GpsDirectory.TagAltitude, out var altRational) == true
-                    ? (gps?.GetInt32(GpsDirectory.TagAltitudeRef) == 1 ? -altRational.ToDouble() : altRational.ToDouble())
-                    : (double?)null;
-                var dateTaken = subIfd?.GetDescription(ExifDirectoryBase.TagDateTimeOriginal)
-                    ?? ifd0?.GetDescription(ExifDirectoryBase.TagDateTimeOriginal)
-                    ?? ifd0?.GetDescription(ExifDirectoryBase.TagDateTime);
-
-                var dto = new ImageInfoDto
-                {
-                    FileName = fileName,
-                    FilePath = "/images/" + fileName,
-                    CameraMake = cameraMake,
-                    CameraModel = cameraModel,
-                    DateTaken = dateTaken,
-                    Latitude = geo?.Latitude,
-                    Longitude = geo?.Longitude,
-                    Altitude = altitude
-                };
-                imageInfoList.Add(dto);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Error reading {File}", file);
-            }
+            var dto = GetImageMetadata(file);
+            if (dto is not null) imageInfoList.Add(dto);
         }
-        if (noSort == "yes") return imageInfoList.Where(i => i.Altitude > 52);
+
+        if (noSort?.ToLower() == "yes") return imageInfoList.Where(i => i.Altitude > 52);
 
         return imageInfoList.OrderBy(i => i.DateTaken);
     }
+
+    public ImageInfoDto? GetImageMetadata(string file)
+    {
+        try
+        {
+            var directories = ImageMetadataReader.ReadMetadata(file);
+            var fileName = Path.GetFileName(file);
+            var ifd0 = directories.OfType<ExifIfd0Directory>().FirstOrDefault();
+            var subIfd = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+            var cameraMake = ifd0?.GetDescription(ExifDirectoryBase.TagMake);
+            var cameraModel = ifd0?.GetDescription(ExifDirectoryBase.TagModel);
+            var gps = directories.OfType<GpsDirectory>().FirstOrDefault();
+            var geo = gps is not null && gps.TryGetGeoLocation(out var loc) ? loc : (GeoLocation?)null;
+            var altitude = gps?.TryGetRational(GpsDirectory.TagAltitude, out var altRational) == true
+                ? (gps?.GetInt32(GpsDirectory.TagAltitudeRef) == 1 ? -altRational.ToDouble() : altRational.ToDouble())
+                : (double?)null;
+            var dateTaken = subIfd?.GetDescription(ExifDirectoryBase.TagDateTimeOriginal)
+                ?? ifd0?.GetDescription(ExifDirectoryBase.TagDateTimeOriginal)
+                ?? ifd0?.GetDescription(ExifDirectoryBase.TagDateTime);
+
+            return new ImageInfoDto
+            {
+                FileName = fileName,
+                FilePath = "/images/" + fileName,
+                CameraMake = cameraMake,
+                CameraModel = cameraModel,
+                DateTaken = dateTaken,
+                Latitude = geo.HasValue ? (decimal?)geo.Value.Latitude : null,
+                Longitude = geo.HasValue ? (decimal?)geo.Value.Longitude : null,
+                Altitude = altitude
+            };
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error reading {File}", file);
+            return null;
+        }
+    }
+
+    public ImageInfoDto? GetImageMetadataByFileName(string fileName)
+    {
+        if (string.IsNullOrEmpty(_env.WebRootPath)) return null;
+        var filePath = Path.Combine(_env.WebRootPath, "images", fileName);
+        if (!File.Exists(filePath)) return null;
+        return GetImageMetadata(filePath);
+    }
+
     private bool IsJpeg(string file)
     {
         var ext = Path.GetExtension(file);
