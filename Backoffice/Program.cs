a44eb3ccc4184c093a6efb630924103438ebcbbb
douglas-c-lobo-alb <douglas-c-lobo@alticelabs.com>
2026-03-16 11:33:37 +0000

    fix: consolidate to single top-level Program.cs, remove duplicate

--- Backoffice/Program.cs
+++ Backoffice/Program.cs
@@ -1,44 +1,36 @@
 using Backoffice.Components;
+using Radzen;
 
-namespace Backoffice;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddHttpClient("ExifApi", client =>
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+});
 
-        builder.Services.AddHttpClient("ExifApi", client =>
-        {
-            client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!);
-        })
-        .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-        {
-            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-        });
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
-        // Add services to the container.
-        builder.Services.AddRazorComponents()
-            .AddInteractiveServerComponents();
+builder.Services.AddRadzenComponents();
 
-        var app = builder.Build();
+var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error", createScopeForErrors: true);
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseStaticFiles();
-        app.UseAntiforgery();
-
-        app.MapRazorComponents<App>()
-            .AddInteractiveServerRenderMode();
-
-        app.Run();
-    }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
