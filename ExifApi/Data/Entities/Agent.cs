693c5ae41b471742a89b09e0f0d5ee4f2a12034c
Douglas Lobo <douglascnlobo@icloud.com>
2026-03-26 09:05:50 +0000

    fix: add Key attribute to Id property in Agent class

--- ExifApi/Data/Entities/Agent.cs
+++ ExifApi/Data/Entities/Agent.cs
@@ -5,6 +5,7 @@ namespace ExifApi.Data.Entities;
 
 public class Agent
 {
+    [Key]
     public int Id { get; set; }
     [Required, MaxLength(255)]
     public string Name { get; set; } = string.Empty;
