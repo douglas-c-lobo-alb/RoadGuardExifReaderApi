2eec310fdbe19cffc67213eec51998ef52a05f88
Douglas Lobo <douglascnlobo@icloud.com>
2026-04-06 20:14:03 +0100

    wip: bootstrap caching configs

--- ExifApi/Infrastructure/Caching/RedisConfig.cs
+++ ExifApi/Infrastructure/Caching/RedisConfig.cs
@@ -4,5 +4,7 @@ namespace ExifApi.Infrastructure.Caching;
 
 public class RedisConfig
 {
-    public string Configuration { get; set; }
+    public string Configuration { get; set; } = string.Empty;
+    public string MultiplexerConfiguration { get; set; } = string.Empty;
+    public int TtlMinutes { get; set; } = 5;
 }
